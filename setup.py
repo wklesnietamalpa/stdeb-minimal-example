e8ed7811f2edc65495c7639d1d6e020211b907fc
wklesnietamalpa <60750633+wklesnietamalpa@users.noreply.github.com>
2024-05-25 19:31:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-import os
-from setuptools import setup
-from nvpy import nvpy
+from setuptools import setup, find_packages
 
 setup(
-    name = "myscript",
-    version = "1.1",
-    author = "Charl P. Botha",
-    author_email = "cpbotha@vxlabs.com",
-    description = "Demo of packaging a Python script as DEB",
-    license = "BSD",
-    url = "https://github.com/cpbotha/stdeb-minimal-example",
-    packages=['myscript'],
-    entry_points = {
-        'console_scripts' : ['myscript = myscript.myscript:main']
+    name="myscript",
+    version="1.1",
+    author="Charl P. Botha",
+    author_email="cpbotha@vxlabs.com",
+    description="Demo of packaging a Python script as DEB",
+    license="BSD",
+    url="https://github.com/cpbotha/stdeb-minimal-example",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['myscript = myscript.myscript:main']
     },
-    data_files = [
+    data_files=[
         ('share/applications/', ['vxlabs-myscript.desktop'])
-        ],
+    ],
     classifiers=[
         "License :: OSI Approved :: BSD License",
     ],
 )
-
