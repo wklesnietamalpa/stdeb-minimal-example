d0c11fdfa81e44668c82d4d16cd029ab066410e1
wklesnietamalpa <60750633+wklesnietamalpa@users.noreply.github.com>
2024-05-25 19:37:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from setuptools import setup, find_packages
+
+# Wczytanie opisu z pliku README
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="myscript",
@@ -6,11 +11,13 @@ setup(
     author="Charl P. Botha",
     author_email="cpbotha@vxlabs.com",
     description="Demo of packaging a Python script as DEB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="BSD",
     url="https://github.com/cpbotha/stdeb-minimal-example",
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['myscript = myscript.myscript:main']
+        'console_scripts': ['myscript=myscript.myscript:main']
     },
     data_files=[
         ('share/applications/', ['vxlabs-myscript.desktop'])
